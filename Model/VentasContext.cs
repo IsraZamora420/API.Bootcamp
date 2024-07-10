016d6d8f1d24d5b7e8bc4c3dc382f8509abbc6ad
IsraZamora420 <israzamo36@gmail@gmail.com>
2024-07-09 19:15:23 -0500

    configuraciones IIS

--- Model/VentasContext.cs
+++ Model/VentasContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace EjemploEntity2.Model;
@@ -42,9 +43,10 @@ public partial class VentasContext : DbContext
     public virtual DbSet<Venta> Ventas { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-J0UV241\\SQLEXPRESS;Database=VENTAS;Integrated Security=True;TrustServerCertificate=True");
 
+    {
+        //optionsBuilder.UseSqlServer("Server=ARTUROPC;Database=MASTERCLASS;Integrated Security=True;TrustServerCertificate=True");
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
--- obj/Debug/net8.0/EjemploEntity2.AssemblyInfo.cs
+++ obj/Debug/net8.0/EjemploEntity2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjemploEntity2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1a11b5d4227b36815cc8d75b733474824c92c57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd8e84338c15e21b6c0b84a2578f31e4fc615271")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjemploEntity2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjemploEntity2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
